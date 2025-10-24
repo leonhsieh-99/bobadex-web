1af59b66f383f36e6093a7f0b32a7e27fd28013b
Leon Hsieh <leonhsieh@yahoo.com>
2025-10-24 05:45:54 -0700

    set up clients and scaffolded auth

---
+++ src/app/auth/actions.ts
@@ -0,0 +1,44 @@
+'use server'
+
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
+import { createClient } from "@/utils/supabase/server"
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/layout')
+  redirect('/')
+}
+
+export async function singup(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    name: formData.get('name') as string,
+    email: formData.get('email') as string,
+    username: formData.get('username') as string,
+    password: formData.get('password') as string
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('layout')
+  redirect('/')
+}
---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import { login } from "../actions";
+
+export default function AuthPage() {
+  return (
+    <form className="">
+      <label htmlFor="email">Email</label>
+      <input id="email" name="email" type="email" required/>
+      <label htmlFor="password">Password</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+    </form>
+  )
+}
